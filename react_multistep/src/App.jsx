96b41b3b547221322623b0436841ea02b011dd2c
LucasFelix013 <luquinhas.pgg@gmail.com>
2023-01-15 21:52:26 -0300

    first commit

---
+++ react_multistep/src/App.jsx
@@ -0,0 +1,60 @@
+//Components
+import { GrFormNext, GrFormPrevious } from "react-icons/gr"; //Import node_modules ICONES
+import {FiSend} from 'react-icons/fi'
+import UserForm from "./components/UserForm";
+import ReviewForm from "./components/ReviewForm";
+import Thanks from "./components/Thanks";
+
+//Hooks
+import { useForm } from "./Hooks/useForm";
+
+import './App.css'
+
+function App() {
+  const formComponents = [<UserForm />, <ReviewForm />, <Thanks />]; 
+  
+  const {currentStep, currentComponent, changeStep, isLastStep, isFirstStep} = useForm(formComponents)
+
+  return (
+    <div className="App">
+      <div className="header">
+      <h2>Deixe sua avaliação</h2>
+       <p>
+        Ficamos felizes com sua compra, utilize o formulário abaixo para avaliar o nosso produto.
+       </p>
+      </div>
+
+       <div className="form-container">
+         <p>etapas</p>
+          <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
+            <div className='inputs-container'>{currentComponent}</div>
+          <div className='actions'>
+
+             {!isFirstStep && (
+               <button type='button' onClick={() => changeStep(currentStep - 1)}>
+               <GrFormPrevious />
+               <span>Voltar</span>
+               </button>
+             )}
+
+              {!isLastStep ? (
+                 <button type='submit'>
+                 <span>Avançar</span> 
+                 <GrFormNext/>
+                   </button>
+              ) : (
+                <button type='button'>
+                <span>Enviar</span> 
+                <FiSend />
+                  </button>
+              )}
+
+          </div>
+         </form>
+       </div>
+      
+    </div>
+  )
+}
+
+export default App
